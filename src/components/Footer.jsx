8c024789351605cdfb97e8f1cb206d3dbcacd325
paras <paraskumarsahu65@gmail.com>
2025-06-07 21:06:07 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer className="bg-white shadow py-4 mt-8">
     <p className="text-center text-gray-500 text-sm">
-      &copy; {new Date().getFullYear()} Your Name. All rights reserved.
+      &copy; {new Date().getFullYear()} Alen sahu Art. All rights reserved.
     </p>
   </footer>
 );
