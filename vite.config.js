82f23f675fe1b734fda58d495bd9cc0146d2d0c9
paras <paraskumarsahu65@gmail.com>
2025-06-07 19:18:05 +0530

    repo name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'AlenSahuArt',
+  base:'/AlenSahuArt/',
   server:{
     port: 3000,
   }
